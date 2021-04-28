098ca0d1e82d9ec3c83a0b14321658d26381b0da
Ibrahim Bashir <ib.bashir@hotmail.com>
2021-04-28 21:04:54 +0100

    Updated about me and portfolio sections of homepage and changed font

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,6 +2,7 @@ import Nav from '../components/Nav';
 import Pocket from '../components/Pocket';
 import LinkedIn from '../img/linkedin.png';
 import Email from '../img/email.png';
+import Me from '../img/me.png';
 
 function Homepage() {
 
@@ -9,15 +10,27 @@ function Homepage() {
         <div className="container">
             <div className="columns">
                 <div className="col content"> 
-                    
+                    <div className="aboutMe">
+                        <img className="photo" src={Me} />
+                        <div className="aboutMeText">
+                            I'm <b>Ibrahim</b>, a <b>Computer Science</b> student at <b>Queen Mary University of London.</b>
+                        </div>
+                    </div>
+                    <div className="aboutMeText">
+                        My main interests are <b>UI / UX</b> and <b>Data Analysis</b>.
+                    </div>
+                    <div className="aboutMeText">
+                        I have experience with <b>React</b>, <b>???</b> and <b>???</b>.
+                    </div>
                 </div>
                 <div className="col content">
-
+                    <a href="" className="portfolioText"><b>Speedcubing App</b></a>
+                    <a href="" className="portfolioText"><b>Expense Claim App</b></a>
                 </div>
                 <div className="col content">
                     <a className="email" href="mailto:ib.bashir@hotmail.com" target="_blank" rel="noopener noreferrer">
                         <img className="contactIcon" src={Email}/>
-                        <div className="contactText">Email Me</div>
+                        <div className="contactText">Email</div>
                     </a>
                     <a className="linkedin" href="https://uk.linkedin.com/in/ibrahim-bashir-250953197" target="_blank" rel="noopener noreferrer">
                         <img className="contactIcon" src={LinkedIn}/>
