fd89bd2975faa3e6b263dcbcf6315f4e496b91eb
Ibrahim Bashir <ib.bashir@hotmail.com>
2021-04-30 15:55:41 +0100

    Updated portfolio links and formatted about me text

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,28 +11,36 @@ function Homepage() {
             <div className="columns">
                 <div className="col content"> 
                     <div className="aboutMe">
-                        <img className="photo" src={Me}/>
+                        <img className="photo" src={Me} alt="Ibrahim Bashir"/>
                         <div className="aboutMeText">
                             I'm <b>Ibrahim</b>, a <b>Computer Science</b> student at <b>Queen Mary University of London.</b>
                         </div>
                     </div>
-                    <div className="aboutMeText">
-                        My main interests are <b>UI / UX</b> and <b>Data Analysis</b>.
+                    <div className="listText">
+                        <b>Interests:</b>
+                        <ul>
+                            <li>UI/UX</li>
+                            <li>Data Analysis</li>
+                        </ul>
                     </div>
-                    <div className="aboutMeText">
-                        I have experience with <b>React</b>, <b>???</b> and <b>???</b>.
+                    <div className="listText">
+                        <b>Skills:</b>
+                        <ul>
+                            <li>React</li>
+                            <li>???</li>
+                        </ul>
                     </div>
                 </div>
                 <div className="col content">
-                    <a className="portfolioText" href="https://ib-rim-speedcube.herokuapp.com/" target="_blank" rel="noopener noreferrer"><b>Speedcubing App</b></a>
+                    <a className="portfolioText" href="https://ib-rim-speedcube.herokuapp.com/" target="_blank" rel="noopener noreferrer">Speedcubing App</a>
                 </div>
                 <div className="col content">
                     <a className="email" href="mailto:ib.bashir@hotmail.com" target="_blank" rel="noopener noreferrer">
-                        <img className="contactIcon" src={Email}/>
+                        <img className="contactIcon" src={Email} alt="Email Icon"/>
                         <div className="contactText">Email</div>
                     </a>
                     <a className="linkedin" href="https://uk.linkedin.com/in/ibrahim-bashir-250953197" target="_blank" rel="noopener noreferrer">
-                        <img className="contactIcon" src={LinkedIn}/>
+                        <img className="contactIcon" src={LinkedIn} alt="LinkedIn Icon"/>
                         <div className="contactText">Linked In</div>
                     </a>	
                 </div>
